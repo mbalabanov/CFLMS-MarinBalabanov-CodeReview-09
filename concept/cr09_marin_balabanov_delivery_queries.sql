ea18e0db817a700a7d1febfa3c5fc795c1c6233f
Marin Balabanov <marincomics@gmail.com>
2020-11-09 09:33:11 +0100

    Addes queries file

---
+++ concept/cr09_marin_balabanov_delivery_queries.sql
@@ -0,0 +1,13 @@
+/* cr09_marin_balabanov_delivery_queries.sql */
+
+/* How many packages/mail were sent from a specific city? */
+
+/* Who sent packages/mail on this date? */
+
+/* Who sent packages/mail between this and that date? */
+
+/* How many packages/mail were sent from a specific country? */
+
+/* Which destinations received their packages/mail between this and that date? */
+
+/* How many packages or letters (mail) got sent in total? */
